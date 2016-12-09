8692d24bc21fe18c45647aa8a5d1e2419a89347e
章华隽 <gavinchangcn@163.com>
2016-12-09 12:21:27 +0800

    替换高德的 SDK 为3D 的地图SDK，实现定位功能

--- app/src/main/kotlin/com/gavin/hzbicycle/ui/MainActivity.kt
+++ app/src/main/kotlin/com/gavin/hzbicycle/ui/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.gavin.hzbicycle.ui
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import com.amap.api.location.AMapLocation
 import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationClientOption
 import com.amap.api.location.AMapLocationListener
-import com.amap.api.maps2d.AMap
-import com.amap.api.maps2d.LocationSource
-import com.amap.api.maps2d.UiSettings
+import com.amap.api.maps.*
+import com.amap.api.maps.model.BitmapDescriptorFactory
+import com.amap.api.maps.model.LatLng
+import com.amap.api.maps.model.MarkerOptions
 import com.gavin.hzbicycle.R
 import com.gavin.hzbicycle.base.BaseActivity
 import com.gavin.hzbicycle.util.LogUtil
-import com.gavin.hzbicycle.util.gsonUtil.GsonUtil
 import com.gavin.hzbicycle.widget.button.NoDoubleClickListener
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.onClick
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+
 
 
 /**
@@ -28,15 +34,20 @@ import org.jetbrains.anko.onClick
  */
 class MainActivity : BaseActivity() {
 
-    val mAMap: AMap by lazy { map.map }
+    val mMapView: MapView by lazy { findViewById(R.id.map) as MapView }
+    val mAMap: AMap by lazy { mMapView.map }
     val mUiSettings: UiSettings by lazy { mAMap.uiSettings }//定义一个UiSettings对象
     //声明AMapLocationClient类对象并初始化
     val mLocationClient: AMapLocationClient by lazy { AMapLocationClient(applicationContext) }
     //声明AMapLocationClientOption对象并初始化
-    val mLocationOption: AMapLocationClientOption by lazy {AMapLocationClientOption()}
+    val mLocationOption: AMapLocationClientOption by lazy { AMapLocationClientOption() }
 
     // 自定义的 View 防双击点击事件
-    val mClickListener : CustomClickListener by lazy { CustomClickListener() }
+    val mClickListener: CustomClickListener by lazy { CustomClickListener() }
+
+    var mLatitude: Double = 0.toDouble() // 纬度
+    var mLongitude: Double = 0.toDouble() // 经度
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -54,19 +65,27 @@ class MainActivity : BaseActivity() {
      */
     fun initAMap(savedInstanceState: Bundle?) {
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
-        map.onCreate(savedInstanceState)
+        mMapView.onCreate(savedInstanceState)
         mAMap.mapType = AMap.MAP_TYPE_NORMAL
         //设置定位回调监听
-        mLocationClient.setLocationListener(CustomLocationListener())
-        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationClient.setLocationListener(mLocationListener)
+
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
         mLocationOption.locationMode = AMapLocationClientOption.AMapLocationMode.Hight_Accuracy
-        //获取一次定位结果：
-        //该方法默认为false。
-        mLocationOption.isOnceLocation = true
         //设置是否返回地址信息（默认返回地址信息）
         mLocationOption.isNeedAddress = true
+        //设置是否只定位一次,默认为false
+        mLocationOption.isOnceLocation = false
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.isWifiActiveScan = true
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.isMockEnable = false
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.interval = 2000
         //给定位客户端对象设置定位参数
         mLocationClient.setLocationOption(mLocationOption)
+        //启动定位  onResume（）中进行定位
+        mLocationClient.startLocation()
 
         mUiSettings.isZoomControlsEnabled = false // 不显示放大缩小按钮
 
@@ -82,29 +101,29 @@ class MainActivity : BaseActivity() {
     override fun onResume() {
         super.onResume()
         //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
-        map.onResume()
+        mMapView.onResume()
         //启动定位
-        mLocationClient.startLocation()
+//        mLocationClient.startLocation()
     }
 
     override fun onPause() {
         super.onPause()
         mLocationClient.stopLocation()//停止定位后，本地定位服务并不会被销毁
         //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
-        map.onPause()
+        mMapView.onPause()
     }
 
     override fun onDestroy() {
         mLocationClient.onDestroy();//销毁定位客户端，同时销毁本地定位服务。
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        map.onDestroy()
+        mMapView.onDestroy()
         super.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
-        map.onSaveInstanceState(outState)
+        mMapView.onSaveInstanceState(outState)
     }
 
     inner class CustomClickListener : NoDoubleClickListener() {
@@ -143,29 +162,47 @@ class MainActivity : BaseActivity() {
      */
     inner class InnerLocationSource : LocationSource {
         override fun deactivate() {
+            LogUtil.i("高德自带定位停止工作了！")
         }
 
         override fun activate(listener: LocationSource.OnLocationChangedListener?) {
-            LogUtil.i("高德自带定位工作了！")
+            LogUtil.i("高德自带定位开始工作了！")
         }
 
     }
+
     /**
      * 自定义高德地图定位监听器
      */
-    inner class CustomLocationListener: AMapLocationListener {
-        override fun onLocationChanged(amapLocation: AMapLocation?) {
-            if (amapLocation != null) {
-                LogUtil.i("自定义高德定位工作了！")
-                LogUtil.i("获取高德定位信息回调：${GsonUtil.INSTANCE.get().toJson(amapLocation)}")
-                if (amapLocation.errorCode == 0) {
+    val mLocationListener = AMapLocationListener { amapLocation ->
+        if (amapLocation != null) {
+            LogUtil.i("自定义高德定位工作了！")
+            if (amapLocation.errorCode == 0) {
                 //可在其中解析amapLocation获取相应内容。
-                }else {
-                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
-                    LogUtil.e("location Error, ErrCode:${amapLocation.errorCode}, errInfo:${amapLocation.errorInfo}")
-                }
+                //定位成功回调信息，设置相关消息
+                val _locationType = amapLocation.locationType // 获取当前定位结果来源，如网络定位结果，详见定位类型表
+                mLatitude = amapLocation.latitude // 获取纬度
+                mLongitude = amapLocation.longitude // 获取经度
+                val _accuracy = amapLocation.accuracy // 获取精度信息)
+                val _df = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                val _date = Date(amapLocation.time)
+                _df.format(_date)//定位时间
+
+                LogUtil.i("获取高德定位信息回调：\n 定位来源：$_locationType \n纬度，经度：($mLatitude，$mLongitude)\n 精度信息：$_accuracy\n 时间：$_date")
+
+                // 设置当前地图显示为当前位置
+                mAMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(mLatitude, mLongitude), 19f))
+                val markerOptions = MarkerOptions()
+                markerOptions.position(LatLng(mLatitude, mLongitude))
+                markerOptions.title("当前位置")
+                markerOptions.visible(true)
+                val bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(resources, R.drawable.main_location_marker))
+                markerOptions.icon(bitmapDescriptor)
+                mAMap.addMarker(markerOptions)
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                LogUtil.e("location Error, ErrCode:${amapLocation.errorCode}, errInfo:${amapLocation.errorInfo}")
             }
         }
-
     }
 }
